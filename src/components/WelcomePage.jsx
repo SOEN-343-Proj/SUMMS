613b377f5aafd277dfcf266492a3dbf9813c1f21
VladimirTodorov21 <arnolito@gmail.com>
2026-02-24 00:19:48 -0500

    Simplified the respository structure

---
+++ src/components/WelcomePage.jsx
@@ -0,0 +1,15 @@
+import '../styles/WelcomePage.css'
+
+function WelcomePage() {
+
+  return (
+    <>
+      <div className="main">
+        <h1>SOEN 343-Project</h1>
+        <h1>Login</h1>
+      </div>
+    </>
+  )
+}
+
+export default WelcomePage
